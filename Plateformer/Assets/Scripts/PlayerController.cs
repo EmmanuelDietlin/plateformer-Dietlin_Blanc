fcfa2670868a1384d0d380fcd06855a12329fc56
EmmanuelDietlin <emmanuel.dietlin@gmail.com>
2022-09-24 15:15:36 +0200

    Start project

---
+++ Plateformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float horizontalGroundSpeed;
+    [SerializeField] private float horizontalAirSpeed;
+    [SerializeField] private float verticalImpulse;
+    [SerializeField] private float DashValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
