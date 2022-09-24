5e45fc239e72564bbc44f1d6d7b0c953c2367178
EmmanuelDietlin <emmanuel.dietlin@gmail.com>
2022-09-24 16:22:50 +0200

    début du travail sur le projet : configuration des axes et ajout des commandes de base

--- Plateformer/Assets/Scripts/PlayerController.cs
+++ Plateformer/Assets/Scripts/PlayerController.cs
@@ -8,15 +8,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float horizontalAirSpeed;
     [SerializeField] private float verticalImpulse;
     [SerializeField] private float DashValue;
+    [SerializeField] private float gravityValue;
+
+
+    private Rigidbody2D rigidBody;
+    private bool isJumping;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
+        {
+            transform.position += Input.GetAxis("Horizontal") * horizontalGroundSpeed * Vector3.right * Time.deltaTime;  
+        }
+        if (Input.GetAxis("Jump") > 0 && !isJumping)
+        {
+            isJumping=true;
+            rigidBody.AddForce(new Vector2(0, verticalImpulse), ForceMode2D.Impulse);
+        }
+        
         
     }
 }
