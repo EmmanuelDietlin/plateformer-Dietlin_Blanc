60fa6f11154013dc4b7b69c04fb3afb516ee4361
Ludovic <ludovic.blanc@telecom-sudparis.eu>
2022-10-04 18:39:49 +0200

    Implémentation du saut à partir d'un mur et de la glissade sur les murs

--- Plateformer/Assets/Scripts/PlayerController.cs
+++ Plateformer/Assets/Scripts/PlayerController.cs
@@ -9,8 +9,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float horizontalAirSpeed;
     [SerializeField] private float verticalImpulse;
     [SerializeField] private float verticalMaxSpeed;
-    [SerializeField] private float DashValue;
     [SerializeField] private float gravityValue;
+    [SerializeField, Range(0, 1)] private float wallFriction;
     [SerializeField] private float fallGravityFactor;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Range(0,20)] private float dashBrake;
     [SerializeField] private float dashValue;
     [SerializeField] private float dashTimer;
+    [SerializeField] private float wallGrabeDuration;
 
 
     
@@ -34,12 +35,16 @@ public class PlayerController : MonoBehaviour
     private bool mayJumpMidAir;
     private string platformTag;
     private float dashStartTime;
+    private float wallGrabStopStartTime;
+    private bool isGrabingWall;
+    private Collider2D groundCollider2D;
     // Start is called before the first frame update
     void Start()
     {
         boxCollider = gameObject.GetComponent<BoxCollider2D>();
         jumpsLeft = jumpNumber;
         dashStartTime = 0f;
+        isGrabingWall = false;
         //Time.timeScale = .1f;
         
     }
@@ -49,9 +54,10 @@ public class PlayerController : MonoBehaviour
     {
         var curTime = Time.time;
         Vector2 groundedBoxCheck = (Vector2)transform.position + new Vector2(0, -.17f);
-        Vector3 boxScale = new Vector3(transform.localScale.x, transform.localScale.y * .69f, transform.localScale.z);
-        isGrounded = Physics2D.OverlapBox(groundedBoxCheck, boxScale, 0, groundLayer);
-        platformTag = isGrounded ? Physics2D.OverlapBox(groundedBoxCheck, boxScale, 0, groundLayer).tag : "";
+        Vector3 boxScale = new Vector3(transform.localScale.x * 0.9f, transform.localScale.y * .69f, transform.localScale.z);
+        groundCollider2D = Physics2D.OverlapBox(groundedBoxCheck, boxScale, 0, groundLayer);
+        isGrounded = groundCollider2D != null;
+        platformTag = isGrounded ? groundCollider2D.tag : "";
         Vector2 wallCollisionsBoxCheck = (Vector2)transform.position;
         isCollidingWallLeft = Physics2D.OverlapBox(wallCollisionsBoxCheck + new Vector2(-.26f, 0f), new Vector3(transform.localScale.x * .5f, transform.localScale.y - .1f, transform.localScale.z), 0, wallLayer);
         isCollidingWallRight = Physics2D.OverlapBox(wallCollisionsBoxCheck + new Vector2(.26f, 0f), new Vector3(transform.localScale.x * .5f, transform.localScale.y - .1f, transform.localScale.z), 0, wallLayer);
@@ -64,19 +70,41 @@ public class PlayerController : MonoBehaviour
         {
             speed.y = platformClipSpeed;
         }
-            
         else if (isCollidingWallRight && isCollidingWallLeft)
         {
             //speed.x = 0;
         }
         else if (isCollidingWallRight)
         {
-            speed.x = Input.GetAxis("Horizontal") > 0 ? 0 : (Input.GetAxis("Horizontal") * currentMaxXSpeed * Vector3.right).x;  
-
+            speed.x = Input.GetAxis("Horizontal") > 0 ? 0 : (Input.GetAxis("Horizontal") * currentMaxXSpeed * Vector3.right).x;
+            if (!isGrabingWall && Input.GetAxis("Horizontal") > 0)
+            {
+                isGrabingWall = true;
+                speed.y = 0;
+                jumpsLeft = 1;
+            }
+            else if(Input.GetAxis("Horizontal") == 0)
+            {
+                if(isGrabingWall)
+                    wallGrabStopStartTime = Time.time;
+                isGrabingWall = false;
+            }
         }
         else if (isCollidingWallLeft)
         {
             speed.x = Input.GetAxis("Horizontal") < 0 ? 0 : (Input.GetAxis("Horizontal") * currentMaxXSpeed * Vector3.right).x;
+            if (!isGrabingWall && Input.GetAxis("Horizontal") < 0)
+            {
+                isGrabingWall = true;
+                speed.y = 0;
+                jumpsLeft = 1;
+            }
+            else if (Input.GetAxis("Horizontal") == 0)
+            {
+                if (isGrabingWall)
+                    wallGrabStopStartTime = Time.time;
+                isGrabingWall = false;
+            }
         } 
         else 
         {
@@ -84,6 +112,10 @@ public class PlayerController : MonoBehaviour
             
             if ((Input.GetAxisRaw("Horizontal") == 0f) && isGrounded) speed.x *= Mathf.Max((1 - inertia*Time.deltaTime),0);
             else speed.x = Mathf.Abs(speed.x) > currentMaxXSpeed ? speed.x : (Input.GetAxis("Horizontal") * currentMaxXSpeed * Vector3.right).x;
+
+            if (isGrabingWall)
+                wallGrabStopStartTime = Time.time;
+            isGrabingWall = false;
         }
 
         if (isGrounded && speed.y <= 0)
@@ -91,13 +123,20 @@ public class PlayerController : MonoBehaviour
             speed.y = 0;
 
         }
-        if (Input.GetAxis("Jump") > 0 && mayJumpMidAir && jumpsLeft > 0)
+        if (Input.GetAxisRaw("Jump") > 0 && mayJumpMidAir && jumpsLeft > 0)
         {
             mayJumpMidAir = false;
             jumpsLeft--;
-            Jump();
+            if (isGrabingWall || curTime < wallGrabStopStartTime + wallGrabeDuration)
+            {
+                WallJump();
+            }
+            else
+            {
+                Jump();
+            }
         }
-        if (Input.GetAxis("Jump") == 0)
+        if (Input.GetAxisRaw("Jump") == 0)
         {
             mayJumpMidAir = true;
             if (speed.y > verticalImpulse * longJumpThreshold) speed.y /= 1.5f;
@@ -114,7 +153,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawCube((Vector2)transform.position + new Vector2(0, -.17f), new Vector3(transform.localScale.x, transform.localScale.y * .69f, transform.localScale.z));
+        // Gizmos.DrawCube((Vector2)transform.position + new Vector2(0, -.17f), new Vector3(transform.localScale.x, transform.localScale.y * .69f, transform.localScale.z));
         //Gizmos.DrawCube((Vector2)transform.position + new Vector2(-.26f, 0f), new Vector3(transform.localScale.x * .5f, transform.localScale.y, transform.localScale.z));
         //Gizmos.DrawCube((Vector2)transform.position + new Vector2(.26f, 0f), new Vector3(transform.localScale.x * .5f, transform.localScale.y, transform.localScale.z));
     }
@@ -126,8 +165,9 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyPhysics()
     {
+        float gravityUsed = isGrabingWall ? gravityValue * wallFriction : gravityValue;
         if (speed.y > 0) speed.y -= gravityValue * Time.deltaTime;
-        else if (speed.y > -verticalMaxSpeed) speed.y -= gravityValue * fallGravityFactor * Time.deltaTime;
+        else if (speed.y > -verticalMaxSpeed) speed.y -= gravityUsed * fallGravityFactor * Time.deltaTime;
         else speed.y = -verticalMaxSpeed;
         
     }
@@ -141,12 +181,24 @@ public class PlayerController : MonoBehaviour
         speed.x += dashValue * movDirX * currentMaxXSpeed;
     }
 
+    private void WallJump()
+    {
+        speed.y = verticalImpulse;
+        dashStartTime = Time.time;
+        float movDirX = Input.GetAxisRaw("Horizontal");
+        if (isCollidingWallRight)
+            movDirX = -1;
+        if(isCollidingWallLeft)
+            movDirX = 1;
+        speed.x += dashValue * movDirX * currentMaxXSpeed;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         ColliderDistance2D distance = boxCollider.Distance(collision);
         if (distance.normal.x != 0)
         {
-            transform.position -= new Vector3(distance.normal.x * Mathf.Abs(distance.distance), 0, 0);
+            transform.position += new Vector3(distance.normal.x * distance.distance, 0, 0);
         }
         if (distance.normal.y != 0)
         {
